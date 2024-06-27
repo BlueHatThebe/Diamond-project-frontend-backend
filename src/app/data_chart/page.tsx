22a29947eefbc495861f336ca53a5b2cee523534
BlueHatThebe <thebzennkhasi@gmail.com>
2024-06-27 12:14:53 +0200

    Styled datachart page

--- src/app/data_chart/page.tsx
+++ src/app/data_chart/page.tsx
@@ -1,42 +1,63 @@
 "use client";
 import React from 'react';
-import styled from 'styled-components'; // Import styled-components for styling
+import styled from 'styled-components';
 
-// Styled component for the card
 const Card = styled.div`
-	background-color: #f0f0f0;
+	background-color: #ddd; 
+	color: #fff;
 	border: 1px solid #eaeaea;
 	border-radius: 8px;
 	box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
 	padding: 20px;
 	margin-bottom: 20px;
+
+	ul {
+		list-style-type: none;
+		padding: 0;
+
+		li {
+			margin-bottom: 10px;
+			border-radius: 8px; 
+			overflow: hidden;
+
+			button {
+				width: 100%;
+				background-color: #000;
+				color: #fff;
+				border: none;
+				padding: 10px;
+				cursor: pointer;
+				transition: background-color 0.3s ease;
+
+				&:hover {
+					background-color: #333;
+				}
+			}
+		}
+	}
 `;
 
 const IndexPage: React.FC = () => {
-	// Define type for sensor URLs
 	type SensorUrls = {
 		[key: string]: string;
 	};
 
-	// URLs for each sensor
 	const sensorUrls: SensorUrls = {
 		"TH Sensor": "https://thebzennkhasi.grafana.net/d/bdq0woolrqygwb/temperature-readings?orgId=1&from=1718791516000&to=1718791554000",
 		"Gas Sensor": "https://thebzennkhasi.grafana.net/d/ddq0yjtggh7gge/gas-sensor-readings?orgId=1&from=1716884097540&to=17194760",
 		"Motion": "https://thebzennkhasi.grafana.net/d/bdq11fygb9y4gf/motion-sensor-readings?orgId=1&from=1719456437103&to=1719478037103",
 	};
 
-	// Function to open URL in a new tab
 	const openUrl = (url: string) => {
 		window.open(url, '_blank');
 	};
 
 	return (
 		<div className='p-10'>
-			<h1 className='text-center'>List of Datacharts</h1>
+			<h1 className='text-center' style={{ color: '#fff' }}>List of Datacharts</h1>
 
 			<Card>
-				<h2>Select an Item</h2>
-				{/* List of clickable buttons */}
+				<h2 style={{ color: '#000' }}>Select an Item</h2> {/* Text color set to black */}
 				<ul>
 					{Object.keys(sensorUrls).map((sensorName, index) => (
 						<li key={index}>
@@ -52,5 +73,3 @@ const IndexPage: React.FC = () => {
 };
 
 export default IndexPage;
-
-
