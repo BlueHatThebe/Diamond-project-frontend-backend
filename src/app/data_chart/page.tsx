121837bf3873c26c25eea6c6d3c8f8c61dd89ca2
MakZwane71 <makhohlisathemba@gmail.com>
2024-06-27 14:11:13 +0200

    linkin to Grafana

--- src/app/data_chart/page.tsx
+++ src/app/data_chart/page.tsx
@@ -1,87 +1,29 @@
-"use client";
-<<<<<<< HEAD
 import React from "react";
-import styled from "styled-components"; // Import styled-components for styling
-=======
-import React from 'react';
-import styled from 'styled-components';
->>>>>>> e416018178003e8dc8a9008bba277b1041a8990c
+import GrafanaGas from "@/components/GrafanaGas";
+import GrafanaMotion from "@/components/GrafanaMotion";
+import GrafanaTemp from "@/components/Grafanatemp";
+import Link from "next/link";
 
-const Card = styled.div`
-	background-color: #ddd; 
-	color: #fff;
-	border: 1px solid #eaeaea;
-	border-radius: 8px;
-	box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-	padding: 20px;
-	margin-bottom: 20px;
-
-	ul {
-		list-style-type: none;
-		padding: 0;
-
-		li {
-			margin-bottom: 10px;
-			border-radius: 8px; 
-			overflow: hidden;
-
-			button {
-				width: 100%;
-				background-color: #000;
-				color: #fff;
-				border: none;
-				padding: 10px;
-				cursor: pointer;
-				transition: background-color 0.3s ease;
-
-				&:hover {
-					background-color: #333;
-				}
-			}
-		}
-	}
-`;
-
-const IndexPage: React.FC = () => {
-	type SensorUrls = {
-		[key: string]: string;
-	};
-
-	const sensorUrls: SensorUrls = {
-		"TH Sensor":
-			"https://thebzennkhasi.grafana.net/d/bdq0woolrqygwb/temperature-readings?orgId=1&from=1718791516000&to=1718791554000",
-		"Gas Sensor":
-			"https://thebzennkhasi.grafana.net/d/ddq0yjtggh7gge/gas-sensor-readings?orgId=1&from=1716884097540&to=17194760",
-		Motion:
-			"https://thebzennkhasi.grafana.net/d/bdq11fygb9y4gf/motion-sensor-readings?orgId=1&from=1719456437103&to=1719478037103",
-	};
-
-	const openUrl = (url: string) => {
-		window.open(url, "_blank");
-	};
-
+const Data_Charts = () => {
 	return (
-		<div className='p-10'>
-<<<<<<< HEAD
-			<h1 className='text-center text-white'>Data-Charts</h1>
-=======
-			<h1 className='text-center' style={{ color: '#fff' }}>List of Datacharts</h1>
->>>>>>> e416018178003e8dc8a9008bba277b1041a8990c
-
-			<Card>
-				<h2 style={{ color: '#000' }}>Select an Item</h2> {/* Text color set to black */}
-				<ul>
-					{Object.keys(sensorUrls).map((sensorName, index) => (
-						<li key={index}>
-							<button onClick={() => openUrl(sensorUrls[sensorName])}>
-								{sensorName}
-							</button>
-						</li>
-					))}
-				</ul>
-			</Card>
+		<div className="bg-gray-300 p-10 rounded shadow-md'">
+			<h2 className='text-black text-center font-bold underline'>
+				Display Graphs
+			</h2>
+			<p className='text-left font-semibold'>press on any to view progress:</p>
+			<div className=' flex flex-col items-start gap-2'>
+				<Link href='/' className='text-black col-start-1 no-underline'>
+					*For Gas Sensor
+				</Link>
+				<Link href='/' className='text-black gap-3 no-underline'>
+					*For Temperature Sensor
+				</Link>
+				<Link href='/' className='text-black gap-3 no-underline'>
+					*For the Motion Sensor
+				</Link>
+			</div>
 		</div>
 	);
 };
 
-export default IndexPage;
+export default Data_Charts;
--- src/components/GrafanaGas.tsx
+++ src/components/GrafanaGas.tsx
@@ -2,12 +2,16 @@ import React from "react";
 
 const GrafanaGas = () => {
 	return (
-		<div style={{ width: "100%", height: "500px" }}>
-			<iframe
-				src={process.env.NEXT_PUBLIC_GRAFANA_EMBED_LINK3}
-				width='100%'
-				height='100%'
-				frameBorder='0'></iframe>
+		<div className='bg-gray-300 p-6 rounded shadow-md'>
+			<div>
+				<div style={{ width: "100%", height: "500px" }}>
+					<iframe
+						src={process.env.NEXT_PUBLIC_GRAFANA_EMBED_LINK3}
+						width='100%'
+						height='100%'
+						frameBorder='0'></iframe>
+				</div>
+			</div>
 		</div>
 	);
 };
