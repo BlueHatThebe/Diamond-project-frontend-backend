1718ea0e6eac92d4077359a73cfcad507b532663
MakZwane71 <makhohlisathemba@gmail.com>
2024-06-27 14:31:55 +0200

    makig slots for graphs

--- src/app/data_chart/page.tsx
+++ src/app/data_chart/page.tsx
@@ -1,29 +1,60 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import GrafanaGas from "@/components/GrafanaGas";
 import GrafanaMotion from "@/components/GrafanaMotion";
 import GrafanaTemp from "@/components/Grafanatemp";
 import Link from "next/link";
 
-const Data_Charts = () => {
+const DataCharts: React.FC = () => {
+	const [showGasGraph, setShowGasGraph] = useState(false);
+	const [showTempGraph, setShowTempGraph] = useState(false);
+	const [showMotionGraph, setShowMotionGraph] = useState(false);
+
+	const toggleGasGraph = () => {
+		setShowGasGraph(!showGasGraph);
+	};
+
+	const toggleTempGraph = () => {
+		setShowTempGraph(!showTempGraph);
+	};
+
+	const toggleMotionGraph = () => {
+		setShowMotionGraph(!showMotionGraph);
+	};
+
 	return (
-		<div className="bg-gray-300 p-10 rounded shadow-md'">
+		<div className='bg-gray-300 p-10 rounded shadow-md'>
 			<h2 className='text-black text-center font-bold underline'>
 				Display Graphs
 			</h2>
-			<p className='text-left font-semibold'>press on any to view progress:</p>
-			<div className=' flex flex-col items-start gap-2'>
-				<Link href='/' className='text-black col-start-1 no-underline'>
-					*For Gas Sensor
+			<p className='text-left font-semibold'>Press on any to view progress:</p>
+			<div className='flex flex-col'>
+				<Link
+					href='#'
+					className='text-black mb-2 no-underline'
+					onClick={toggleGasGraph}>
+					For Gas Sensor
 				</Link>
-				<Link href='/' className='text-black gap-3 no-underline'>
-					*For Temperature Sensor
+				{showGasGraph && <GrafanaGas />}
+
+				<Link
+					href='#'
+					className='text-black mb-2 no-underline'
+					onClick={toggleTempGraph}>
+					For Temperature Sensor
 				</Link>
-				<Link href='/' className='text-black gap-3 no-underline'>
-					*For the Motion Sensor
+				{showTempGraph && <GrafanaTemp />}
+
+				<Link
+					href='#'
+					className='text-black mb-2 no-underline'
+					onClick={toggleMotionGraph}>
+					For the Motion Sensor
 				</Link>
+				{showMotionGraph && <GrafanaMotion />}
 			</div>
 		</div>
 	);
 };
 
-export default Data_Charts;
+export default DataCharts;
