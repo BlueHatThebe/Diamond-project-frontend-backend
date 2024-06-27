3a1b0d00d75b88e1f9060539a3ec86c56efd2590
BlueHatThebe <thebzennkhasi@gmail.com>
2024-06-27 16:42:10 +0200

    added links

--- src/components/GrafanaMotion.tsx
+++ src/components/GrafanaMotion.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const GrafanaMotion = () => {
 	return (
 		<div>
-			<Link href='' className='text-black no-underline'>
+			<Link href='https://thebzennkhasi.grafana.net/public-dashboards/f85b6b262fd446da9b4401b604eb8927' className='text-black no-underline'>
 				*Motion Sensor
 			</Link>
 		</div>
