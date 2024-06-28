8d0996d150202c3ec6f5dc1113f6686041cee9f0
BlueHatThebe <thebzennkhasi@gmail.com>
2024-06-28 14:48:55 +0200

    step1

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,94 +1,15 @@
 "use client";
-import React, { useEffect } from "react";
+import React from "react";
 import Link from "next/link";
 import { signOut, useSession } from "next-auth/react";
+import { FaArrowAltCircleLeft } from "react-icons/fa";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 const Navbar = () => {
 	const { data: session } = useSession();
 
-<<<<<<< HEAD
-	useEffect(() => {
-		// Manually initialize Bootstrap dropdowns
-		const dropdownToggleList = [].slice.call(
-			document.querySelectorAll('[data-bs-toggle="dropdown"]')
-		);
-		dropdownToggleList.map(function (dropdownToggle) {
-			return new bootstrap.Dropdown(dropdownToggle);
-		});
-	}, []);
-
 	return (
 		<nav className='navbar navbar-dark navbar-expand-lg bg-dark'>
-			<div className='container-fluid d-flex justify-content-between align-items-center'>
-				<div className='d-flex align-items-center'>
-					{!session ? (
-						<Link href='/' passHref>
-							<span className='nav-link text-white'>Home</span>
-						</Link>
-					) : (
-						<Link href='/dashboard' passHref>
-							<span className='nav-link text-white'>Dashboard</span>
-						</Link>
-					)}
-				</div>
-				<div className='text-center text-white font-bold'>Shiny Secure</div>
-				<ul className='nav d-flex gap-3'>
-					{session ? (
-						<li className='nav-item dropdown'>
-							<div
-								className='nav-link dropdown-toggle text-white'
-								id='navbarDropdownMenuAvatar'
-								role='button'
-								data-bs-toggle='dropdown'
-								aria-expanded='false'
-								onClick={(e) => e.preventDefault()}>
-								<img
-									src=''
-									className='rounded-circle'
-									height='15'
-									alt='Avatar'
-									loading='lazy'
-								/>
-							</div>
-							<ul
-								className='dropdown-menu dropdown-menu-end'
-								aria-labelledby='navbarDropdownMenuAvatar'>
-								<li>
-									<Link href='/profile'>
-										<span className='dropdown-item'>My Profile</span>
-									</Link>
-								</li>
-								<li>
-									<span className='dropdown-item' onClick={() => signOut()}>
-										Logout
-									</span>
-								</li>
-							</ul>
-						</li>
-					) : (
-						<>
-							<li className='nav-item'>
-								<Link href='/login' passHref>
-									<span className='nav-link text-white'>Login</span>
-								</Link>
-							</li>
-							<li className='nav-item'>
-								<Link href='/register' passHref>
-									<span className='nav-link text-white'>Register</span>
-								</Link>
-							</li>
-						</>
-					)}
-				</ul>
-			</div>
-		</nav>
-	);
-=======
-<<<<<<< HEAD
-	return (
-		<nav className='navbar navbar-dark navbar-expand-lg bg-dark fixed top-2 left- right-2'>
 			<div className='container-fluid d-flex justify-content-between align-items-center'>
 				<div className='d-flex align-items-center'>
 					{!session ? (
@@ -136,84 +57,6 @@ const Navbar = () => {
 			</div>
 		</nav>
 	);
-=======
-    useEffect(() => {
-        // Manually initialize Bootstrap dropdowns
-        const dropdownToggleList = [].slice.call(document.querySelectorAll('[data-bs-toggle="dropdown"]'));
-        dropdownToggleList.map(function (dropdownToggle) {
-            return new bootstrap.Dropdown(dropdownToggle);
-        });
-    }, []);
-
-    return (
-        <nav className='navbar navbar-dark navbar-expand-lg bg-dark'>
-            <div className='container-fluid d-flex justify-content-between align-items-center'>
-                <div className='d-flex align-items-center'>
-                    {!session ? (
-                        <Link href='/' passHref>
-                            <span className='nav-link text-white'>Home</span>
-                        </Link>
-                    ) : (
-                        <Link href='/dashboard' passHref>
-                            <span className='nav-link text-white'>Dashboard</span>
-                        </Link>
-                    )}
-                </div>
-                <div className='text-center text-white font-bold'>Shiny Secure</div>
-                <ul className='nav d-flex gap-3'>
-                    {session ? (
-                        <li className='nav-item dropdown'>
-                            <div
-                                className="nav-link dropdown-toggle text-white"
-                                id="navbarDropdownMenuAvatar"
-                                role="button"
-                                data-bs-toggle="dropdown"
-                                aria-expanded="false"
-                                onClick={(e) => e.preventDefault()}
-                            >
-                                <img
-                                    src="https://cdn.create.vista.com/api/media/small/475532336/stock-vector-people-icon-person-icon-user-icon-vector-illustration-graphic-design"
-                                    style={{
-                                        width: '50px',
-                                        height: '50px',
-                                        borderRadius: '50%', 
-                                        objectFit: 'fill', 
-                                    }}
-                                    alt="Avatar"
-                                    loading="lazy"
-                                />
-                            </div>
-                            <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownMenuAvatar">
-                                <li>
-                                    <Link href="/profile">
-                                        <span className="dropdown-item">My Profile</span>
-                                    </Link>
-                                </li>
-                                <li>
-                                    <span className="dropdown-item" onClick={() => signOut()}>Logout</span>
-                                </li>
-                            </ul>
-                        </li>
-                    ) : (
-                        <>
-                            <li className='nav-item'>
-                                <Link href='/login' passHref>
-                                    <span className='nav-link text-white'>Login</span>
-                                </Link>
-                            </li>
-                            <li className='nav-item'>
-                                <Link href='/register' passHref>
-                                    <span className='nav-link text-white'>Register</span>
-                                </Link>
-                            </li>
-                        </>
-                    )}
-                </ul>
-            </div>
-        </nav>
-    );
->>>>>>> origin
->>>>>>> d7d165608b75d21b5653a3eaf814c48be93b8bc1
 };
 
 export default Navbar;
