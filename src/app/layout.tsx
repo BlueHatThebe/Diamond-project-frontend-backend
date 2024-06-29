24f4b8c31f2d8f59d5d2a90a6f71e24d8fb56f44
MakZwane71 <makhohlisathemba@gmail.com>
2024-06-29 10:18:18 +0200

    Changes in the profile page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default async function RootLayout({
 						<div className='text-2xl gap-2'>
 							<Navbar />
 						</div>
-						<div className=' ml-2 mr-2 mx-auto  text-2xl gap-2 mb-10'>
+						<div className=' ml-2 mr-2 mx-auto max-w-5xl text-2xl gap-2 mb-10'>
 							{children}
 						</div>
 						<div className=''>
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,27 +1,29 @@
 import React from "react";
-import Avatar from "@/components/avatar";
+import Cookies from "js-cookie";
 
 const ProfilePage: React.FC = () => {
+	const userEmail = Cookies.get("userEmail");
+	const userCompany = Cookies.get("userCompany");
+
 	return (
 		<div>
-			<h2 className='text-center text-4xl underline font-semibold mb-6 text-white'>
-				Employee Profile
-			</h2>
-			<div className=' flex items-center justify-center'>
+			<h2 className='text-center text-4xl mb-6 text-white'>Profile</h2>
+			<div className='flex items-center justify-center'>
 				<div className='bg-dark text-white rounded-lg shadow-md p-6 w-full max-w-3xl'>
 					<div className='flex flex-col items-center'>
-						<Avatar
+						<img
 							src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSLU5_eUUGBfxfxRd4IquPiEwLbt4E_6RYMw&s'
 							alt='User Avatar'
+							className='rounded-full mb-4'
 						/>
-						<h1 className='text-xl font-bold mb-2'>John Doe</h1>
+						<h1 className='text-xl font-bold mb-2'>User Profile</h1>
 						<div className='w-full bg-white rounded-lg p-4 mb-4'>
 							<h4 className='text-gray-900 text-lg'>Company:</h4>
-							<p className='text-gray-700'>Your Company Name</p>
+							<p className='text-gray-700'>{userCompany}</p>
 						</div>
 						<div className='w-full bg-white rounded-lg p-4'>
-							<h4 className='text-gray-900 text-lg'>Employee Details:</h4>
-							<p className='text-gray-700'>Details about the employee</p>
+							<h4 className='text-gray-900 text-lg'>Email:</h4>
+							<p className='text-gray-700'>{userEmail}</p>
 						</div>
 					</div>
 				</div>
